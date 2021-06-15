b3f1eb0d466f09a678a4e29e2fd369e540eaecea
BiXtico <56024802+BiXtico@users.noreply.github.com>
2021-06-16 00:25:45 +0200

    :construction: updating the authentication page

--- app/src/main/java/com/example/nutritiontrack/ui/auth/auth.kt
+++ app/src/main/java/com/example/nutritiontrack/ui/auth/auth.kt
@@ -8,24 +8,24 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.nutritiontrack.R
 
-class login : Fragment() {
+class auth : Fragment() {
 
     companion object {
-        fun newInstance() = login()
+        fun newInstance() = auth()
     }
 
-    private lateinit var viewModel: LoginViewModel
+    private lateinit var viewModel: AuthViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.login_fragment, container, false)
+        return inflater.inflate(R.layout.auth_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
